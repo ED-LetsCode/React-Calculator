ef4e19b204cd92d18526d3554de4e697d3189863
ED-LetsCode <eray.demirel@lets-code.at>
2023-03-06 21:26:42 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen flex justify-center items-center flex-col bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <h1 className="text-4xl mb-4 text-white font-bold">Calculator</h1>
+      <div className="bg-slate-200 h-[600px] w-[500px] rounded-md shadow-md p-4 font-bold text-2xl">
+        <div className=" bg-slate-100 rounded-t-md h-20 w-full shadow-md flex justify-end items-center pr-3">
+          <h1>3243</h1>
+        </div>
+        <div className="w-full h-[480px] mt-3">
+          <div className="flex justify-between"></div>
+          <div className="grid grid-cols-3 gap-2 mt-3"></div>
+        </div>
+      </div>
     </div>
   );
 }
